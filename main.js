26950487459683c6a896ae68a4fd9aa3ee6406e7
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-11-06 17:12:20 +0200

    Added reading reminders

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ console.log("Hello! My name is Aid.");
 console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-// use input() + to get the user's output
+let name = input();
 
-const your_name = input();
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
 
-console.log('What a great name you have', (your_name)+ '!');
+// reading all remainders
 
+const remainder3 = parseInt(input());
+const remainder5 = parseInt(input());
+const remainder7 = parseInt(input());
+
+let your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+console.log("Your age is", your_age, "; that's a good time to start programming!");
