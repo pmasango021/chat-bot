fd00a344f9af41084e608b07a21aa829fa624737
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-11-06 16:49:46 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const input = require('sync-input');
+
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
+
+// use input() + to get the user's output
+
+const your_name = input();
+
+console.log('What a great name you have', (your_name)+ '!');
+
